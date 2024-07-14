ab2e11c9c888c6ffa8695290f2feeb9bff604241
CryptMick <mickking3020@gmail.com>
2024-07-14 07:27:36 +0100

    feat: changed game decision rule. Earlier decision rule with switch conditional statement led to a bug, the newly introduced method would handle decision better and increase winner score as game rounds continued

--- func.js
+++ func.js
@@ -30,61 +30,41 @@ function getHumanChoice() {
 }
 
 // Using Math.random() to initiate a random choice for computer
-console.log("Computer Choice: ");
-let comChoice;
-function getComputerChoice() {
-    let compChoice = Math.random();
-    console.log(compChoice);
-    if (compChoice <= 0.35) {
-        comChoice = "ROCK";
-        console.log(comChoice);
-    } else if (compChoice >= 0.36 && compChoice <= 0.65) {
-        comChoice = "PAPER";
-        console.log(comChoice);
-    } else if (compChoice >= 0.66 && compChoice <= 1) {
-        comChoice = "SCISSORS";
-        console.log(comChoice);
+let replay;
+console.log("Round 1:");
+function playRound(humanChoice, computerChoice) {
+    console.log("Decision Making");
+    if (hChoice === "ROCK" && comChoice === "PAPER") {
+        console.log("You Lose! Rock beat Paper");
+            ++computerScore;
+            replay = "SUCCESS";
+    } else if (hChoice === "PAPER" && comChoice === "SCISSORS") {
+        console.log("You Lose! Scissors beats Paper '(it cut paper into pieces)'");
+        ++computerScore;
+        replay = "SUCCESS";
+    } else if (hChoice === "SCISSORS" && comChoice === "ROCK"){
+        console.log("You Lose! Rock smash Scissors ");
+            ++computerScore;
+            replay = "SUCCESS";
+    } else if (hChoice === "PAPER" && comChoice === "ROCK") {
+        console.log("You Win! Paper suffocate Rock to death");
+            ++humanScore;
+            replay = "SUCCESS";
+    } else if ( hChoice === "SCISSORS" && comChoice === "PAPER") {
+        console.log("You Win! Scissors beats Paper");
+            ++humanScore;
+            replay = "SUCCESS";
+    } else if (hChoice === "ROCK" && comChoice === "SCISSORS") {
+        console.log("You win! Rock smash Scisstors");
+            ++humanScore;
+            replay = "SUCCESS";
     } else {
-        console.log("No Decision");
+        console.log("It's a draw");
+        replay = "SUCCESS";
     }
 }
 
 
-// playRound determines if game has a win or draw
-function playRound(humanChoice, computerChoice) {
-    console.log("Decision Making");
-   /* switch (humanChoice, computerChoice) {
-        case "ROCK", "PAPER":
-            console.log("You Lose! Rock beat Paper");
-            ++computerScore;
-            console.log()
-        break;
-        case "PAPER", "SCISSORS": 
-        console.log("You Lose! Scissors beats Paper '(it cut paper into pieces)'");
-        ++computerScore;
-        break;
-        case "SCISSORS", "ROCK":
-            console.log("You Lose! Rock smash Scissors ");
-            ++computerScore;
-        break;
-        case "PAPER", "ROCK":
-            console.log("You Win! Paper suffocate Rock to death");
-            ++humanScore;
-        break; 
-        case "SCISSORS", "PAPER":
-            console.log("You Win! Scissors beats Paper");
-            ++humanScore;
-        break;
-        case "ROCK", "SCISSORS":
-            console.log("You win! Rock smash Scisstors");
-            ++humanScore;
-        break;
-        default:
-            console.log("No Winner");
-    }*/
-} 
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-console.log(humanScore);
 playRound(humanSelection, computerSelection);
