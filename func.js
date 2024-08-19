dc94f89bdb740eb67c6960f211280e7db67d3fd9
CryptMick <mickking3020@gmail.com>
2024-08-19 15:23:18 +0100

    fix: Updated UI and DOM gameplay Game will now be played without opening browser console with prompt removed, it takes only clicking button to play game.

--- func.js
+++ func.js
@@ -1,152 +1,113 @@
-// Rock Paper Scissors Game ( RPS )
-console.log("Hello World!");
-const start = document.querySelector("#start");
-start.addEventListener("click", () => { console.log("who Please?");
-// Game logic starts here:
-let humanScore, computerScore;
-humanScore = 0;
-computerScore = 0; 
-// Decision for human choice:
-let hChoice;
-console.log("Human Choice is: ");
-function getHumanChoice() {
-    let choice = prompt(" Make a choice here: ('rock' 'paper' or 'scissors')").toUpperCase();
-    switch(choice) {
-
-        case "ROCK":
-            hChoice = "ROCK";
-            console.log(hChoice);
-        break;
-        case "PAPER":
-            hChoice = "PAPER";
-            console.log(hChoice);
-        break;
-        case "SCISSORS":
-            hChoice = "SCISSORS"
-            console.log(hChoice);
-        break;
-        default: 
-        console.log("Indecissive ");
-    }
-}
-
+// Game logic begins here: 
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+let para = document.querySelector(".para");
+let humanscor = document.querySelector("#humanscore");
+let compscore = document.querySelector("#computerscore");
+const message = document.querySelector("#message");
+let playerChoice;
+let humanOutput = document.createElement("p");
+let compOutput = document.createElement("p");
+let div = document.querySelector("#container");
+div.appendChild (humanOutput);
+div.appendChild (compOutput);
 // Using Math.random() to initiate a random choice for computer
-console.log("Computer Choice: ");
 let comChoice;
 function getComputerChoice() {
-    let compChoice = Math.random();
-    console.log(compChoice);
-    if (compChoice <= 0.35) {
+    let rand = Math.random();
+    console.log(rand);
+    if (rand <= 0.35) {
         comChoice = "ROCK";
         console.log(comChoice);
-    } else if (compChoice >= 0.36 && compChoice <= 0.65) {
+    } else if (rand >= 0.36 && rand <= 0.65) {
         comChoice = "PAPER";
         console.log(comChoice);
-    } else if (compChoice >= 0.66 && compChoice <= 1) {
+    } else if (rand >= 0.66 && rand <= 1) {
         comChoice = "SCISSORS";
         console.log(comChoice);
-    } else {
-        console.log("No Decision");
-    }
+    } 
+     compOutput.textContent = `Computer choose: ${comChoice}`;
+     return comChoice;
 }
 
-// Using Math.random() to initiate a random choice for computerremark;
-console.log("Round 1:");
-function playRound(humanChoice, computerChoice) {
-    console.log("Decision Making");
-    if (hChoice === "ROCK" && comChoice === "PAPER") {
-        console.log("You Lose! Rock beat Paper");
-            ++computerScore;
-            remark = "SUCCESS";
-    } else if (hChoice === "PAPER" && comChoice === "SCISSORS") {
-        console.log("You Lose! Scissors beats Paper '(it cut paper into pieces)'");
-        ++computerScore;
-        remark = "SUCCESS";
-    } else if (hChoice === "SCISSORS" && comChoice === "ROCK"){
-        console.log("You Lose! Rock smash Scissors ");
-            ++computerScore;
-            remark = "SUCCESS";
-    } else if (hChoice === "PAPER" && comChoice === "ROCK") {
-        console.log("You Win! Paper suffocate Rock to death");
-            ++humanScore;
-            remark = "SUCCESS";
-    } else if ( hChoice === "SCISSORS" && comChoice === "PAPER") {
-        console.log("You Win! Scissors beats Paper");
-            ++humanScore;
-            remark = "SUCCESS";
-    } else if (hChoice === "ROCK" && comChoice === "SCISSORS") {
-        console.log("You win! Rock smash Scisstors");
-            ++humanScore;
-            remark = "SUCCESS";
-    } else {
-        console.log("It's a draw");
-        remark = "SUCCESS";
-    }
-}
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
-
-// rePlay 
-function playGame() {
-    switch (remark) {
-        case "SUCCESS":
-            console.log("Round 2")
-            const humanSelect = getHumanChoice();
-            const computerSelect = getComputerChoice();
-            playRound(humanSelect, computerSelect);
-                    remark = "Round 3";
-                    switch (remark) {
-                        case "Round 3":
-                            console.log(remark, "Begins:")
-                            const human = getHumanChoice();
-                            const computer = getComputerChoice();
-                            playRound(human, computer);
-                            remark = "Round 4";
-                            switch (remark) {
-                                case "Round 4":
-                                    console.log(remark, "Begins:")
-                                    const human = getHumanChoice();
-                                    const computer = getComputerChoice();
-                                    playRound(human, computer);
-                                    remark = "Round 5";
-                                    switch (remark) {
-                                        case "Round 5":
-                                            console.log(remark, "Begins:")
-                                            const human = getHumanChoice();
-                                            const computer = getComputerChoice();
-                                            playRound(human, computer);
-                                    }
-                            }
-                    }
-        break;
-        default: 
-        console.log("It all end here");
-        }
-        
-}
-playGame();
-
-console.log("Winners Score goes here (Human : Computer");
-console.log(humanScore, ":", computerScore);
-// This section will output result of the game to on the main DOM
-const humanscore = document.querySelector("#humanscore");
-const compscore = document.querySelector("#computerscore");
-const sc = document.createTextNode( humanScore);
-const cs = document.createTextNode( computerScore);
-humanscore.appendChild(sc);
-compscore.appendChild(cs);
-// Game conclusion (win, lose, or a draw game);
- 
-const message = document.querySelector("#message");
-if (humanScore > computerScore) {
-    message.textContent = `Congratulation Player, you won the entire game rounds with a socre of ${humanScore} `;
+let humanScore = 0;
+    let computerScore = 0;
+// Game Decision Rule, playRound determine who win or lose the game 
+function playRound(player, computer) {
     
-} else if (humanScore < computerScore) {
-    message.textContent = `You lose the Game. Therefore Computer wins with a score of ${computerScore}`;
-} else {
-    message.textContent = "It is a draw game. No winner/loser";
+    if (player === "ROCK" && computer === "PAPER") {
+        para.textContent = "You Lose!"; //Rock beat Paper";
+        compscore.textContent = ++computerScore;
+    } else if (player === "PAPER" && computer === "SCISSORS") {
+        para.textContent = "You Lose!"; //Scissors beats Paper '(it cut paper into pieces)'";
+        compscore.textContent = ++computerScore;
+            } else if (player === "SCISSORS" && computer === "ROCK"){
+        para.textContent = "You Lose!"; //Rock smash Scissors ";
+        compscore.textContent = ++computerScore;
+           
+    } else if (player === "PAPER" && computer === "ROCK") {
+        para.textContent = "You Win!"; //Paper suffocate Rock to death";
+        humanscor.textContent = ++humanScore;
+    } else if ( player === "SCISSORS" && computer === "PAPER") {
+        para.textContent = "You Win!"; //Scissors beats Paper";
+        humanscor.textContent = ++humanScore;
+    } else if (player === "ROCK" && computer === "SCISSORS") {
+        para.textContent = "You win!"; //Rock smash Scisstors";
+        humanscor.textContent = ++humanScore;
+    } else {
+        para.textContent = "It's a draw";
+    }
+    return para.textContent;
 }
+humanscor.textContent = humanScore;
+compscore.textContent = computerScore;
+// game() return player and computer input and call the playRound() to deteremine win, lose, or draw.
+
+/*function count() {
+    if (para.textContent === "You win!" ) {humanscor.textContent = ++humanScore;}
+    else if (para.textContent === "You Lose!" ) { compscore.textContent = ++computerScore;}
+}*/
+// Scores and final game message: 
+function scores() {
+    if (humanScore === 5 || computerScore === 5) {
+        alert ("Game Over");
+        if (humanScore > computerScore) {
+            message.textContent = `Congratulation Player, you won the entire game rounds with a socre of ${humanScore} `;
+            
+        } else if (humanScore < computerScore) {
+            message.textContent = `You lose the Game. Therefore Computer wins with a score of ${computerScore}`;
+        } else {
+            message.textContent = "It is a draw game. No winner/loser";
+        }
+    }
+
+}
+
+let computerChoice;
+
+// Player decision is captured in choice 
+rock.addEventListener("click", () => {
+    playerChoice = "ROCK";
+    humanOutput.textContent = `You choose: ${playerChoice}`;
+    computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+    scores();
+    //count(); 
 });
+    paper.addEventListener("click", () => {
+        playerChoice = "PAPER";
+        humanOutput.textContent = `You choose: ${playerChoice}`;
+        computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+        scores();
+        //count();
+    });
+    scissors.addEventListener("click", () => {
+        playerChoice = "SCISSORS";
+        humanOutput.textContent = `You choose: ${playerChoice}`;
+        computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+        scores();
+        //count();
+    });
